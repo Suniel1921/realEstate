055559d81abd4b145d1a6068511e9e02e2ae008a
sunil <logins697@gmail.com>
2024-05-16 11:14:04 +0545

    changes in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,21 +6,21 @@
 //   plugins: [react()],
 // })
 
-// import { defineConfig } from 'vite';
-// import reactRefresh from '@vitejs/plugin-react-refresh';
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [reactRefresh()],
-// });
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [reactRefresh()],
+});
 
 // vite.config.js
-import { defineConfig } from 'vite';
+// import { defineConfig } from 'vite';
 
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      external: /^(@ant-design\/icons.*)$/, // Corrected external path
-    },
-  },
-});
+// export default defineConfig({
+//   build: {
+//     rollupOptions: {
+//       external: /^(@ant-design\/icons.*)$/, // Corrected external path
+//     },
+//   },
+// });
