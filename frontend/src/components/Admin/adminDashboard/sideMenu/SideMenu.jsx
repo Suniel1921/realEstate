3a95d8150dca5626cd2ae6ab1fa24d15c971e40e
sunil <logins697@gmail.com>
2024-05-16 11:39:14 +0545

    chanage antdesing in sideMenu component

--- frontend/src/components/Admin/adminDashboard/sideMenu/SideMenu.jsx
+++ frontend/src/components/Admin/adminDashboard/sideMenu/SideMenu.jsx
@@ -1,43 +1,39 @@
-import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import { FaHome, FaUserPlus } from "react-icons/fa";
-import '../sideMenu/sidemenu.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa'; // Import only the specific icons you need
+import '../sideMenu/sidemenu.css';
 
 const SideMenu = () => {
   return (
-    <>
     <div className='sideMenu_container'>
-        <div className="sideItems">
-            <Link to={'/'} className='listItems'>
-                <FaHome/>
-                <span>Home</span>
-            </Link>
-            <Link to={'/dashboard/admin'} className='listItems'>
-                <FaHome/>
-                <span>Analytics</span>
-            </Link>
-            <Link to={'/dashboard/admin/createRoom'} className='listItems'>
-                <FaHome/>
-                <span>Add Property</span>
-            </Link>
-            <Link to={'/dashboard/admin/manageProperty'} className='listItems'>
-                <FaHome/>
-                <span>Manage Property</span>
-            </Link>
-            <Link to={'/dashboard/admin/createCategory'} className='listItems'>
-                <FaHome/>
-                <span>Create Category</span>
-            </Link>
-            <Link to={'/dashboard/admin/createCategoryPurpose'} className='listItems'>
-                <FaHome/>
-                <span>Create Category Purpose</span>
-            </Link>
-        </div>
-
+      <div className="sideItems">
+        <Link to={'/'} className='listItems'>
+          <FaHome />
+          <span>Home</span>
+        </Link>
+        <Link to={'/dashboard/admin'} className='listItems'>
+          <FaHome />
+          <span>Analytics</span>
+        </Link>
+        <Link to={'/dashboard/admin/createRoom'} className='listItems'>
+          <FaHome />
+          <span>Add Property</span>
+        </Link>
+        <Link to={'/dashboard/admin/manageProperty'} className='listItems'>
+          <FaHome />
+          <span>Manage Property</span>
+        </Link>
+        <Link to={'/dashboard/admin/createCategory'} className='listItems'>
+          <FaHome />
+          <span>Create Category</span>
+        </Link>
+        <Link to={'/dashboard/admin/createCategoryPurpose'} className='listItems'>
+          <FaHome />
+          <span>Create Category Purpose</span>
+        </Link>
+      </div>
     </div>
-      
-    </>
-  )
-}
+  );
+};
 
-export default SideMenu
+export default SideMenu;
